012579921d418f5c3b96f2dffd26f1ae0c354492
jcarmichael9 <jcarmichael802@gmail.com>
2025-05-12 10:40:16 -0400

    im thinking of makin it star wars instead of philly

--- js/roster.js
+++ js/roster.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const grid = document.getElementById('rosterGrid')
+    const grid = document.getElementById('roster-grid')
     const render = list => {
         grid.innerHTML = ''
-        list.forEach() 
+        list.forEach(p => {
+            const col = document.createElement('div')
+            col.className = 'col-6 col-lg-2'
+            col.innerHTML = `
+             <div class="card h-100 shadow-sm">
+                    <img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top">
+                    <div class="card-body text-center">
+                    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
+                    <div class="badge badge-position badge-pos-${p.position}">${p.position}</div>
+                    <p class="small text-muted mb-0">Age: ${p.age}</p>
+                    </div>
+            </div>
+             `
+             grid.appendChild(col)
+        })
     }
 
     render(players)
