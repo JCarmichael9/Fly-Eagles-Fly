3db7a8a97d6a72974e6fae5e249882a6be518c4a
jcarmichael9 <jcarmichael802@gmail.com>
2025-05-12 10:18:52 -0400

    ts gonna be heat

---
+++ js/players.js
@@ -0,0 +1,37 @@
+const players = [
+    {
+        firstName: 'a',
+        lastName: 's',
+        position: 'b',
+        age: 1,
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'p',
+        lastName: 'l',
+        position: 'an',
+        age: 4,
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 't',
+        lastName: 'u',
+        position: 'l',
+        age: 6,
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'v',
+        lastName: 'n',
+        position: 'j',
+        age: 2,
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'f',
+        lastName: 'f',
+        position: 'f',
+        age: 4,
+        photo: 'https://picsum.photos/300/300'
+    }
+]
---
+++ js/roster.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.getElementById('rosterGrid')
+    const render = list => {
+        grid.innerHTML = ''
+        list.forEach() 
+    }
+
+    render(players)
+})
