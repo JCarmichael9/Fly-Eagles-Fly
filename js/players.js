e6acf5a735184048d3aa027eb82bc522104ae257
jcarmichael9 <jcarmichael802@gmail.com>
2025-05-14 09:26:15 -0400

    idk yet

--- js/players.js
+++ js/players.js
@@ -28,5 +28,155 @@ const players = [
         lastName: 'f',
         side: 'f',
         photo: 'https://picsum.photos/300/300'
-    }
+    },
+        {
+        firstName: 'a',
+        lastName: 's',
+        side: 'b',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'p',
+        lastName: 'l',
+        side: 'an',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 't',
+        lastName: 'u',
+        side: 'l',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'v',
+        lastName: 'n',
+        side: 'j',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+     {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+        {
+        firstName: 'a',
+        lastName: 's',
+        side: 'b',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'p',
+        lastName: 'l',
+        side: 'an',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 't',
+        lastName: 'u',
+        side: 'l',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'v',
+        lastName: 'n',
+        side: 'j',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+     {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+        {
+        firstName: 'a',
+        lastName: 's',
+        side: 'b',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'p',
+        lastName: 'l',
+        side: 'an',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 't',
+        lastName: 'u',
+        side: 'l',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'v',
+        lastName: 'n',
+        side: 'j',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+     {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+        {
+        firstName: 'a',
+        lastName: 's',
+        side: 'b',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'p',
+        lastName: 'l',
+        side: 'an',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 't',
+        lastName: 'u',
+        side: 'l',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'v',
+        lastName: 'n',
+        side: 'j',
+        photo: 'https://picsum.photos/300/300'
+    },
+    {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+     {
+        firstName: 'f',
+        lastName: 'f',
+        side: 'f',
+        photo: 'https://picsum.photos/300/300'
+    },
+        {
+        firstName: 'a',
+        lastName: 's',
+        side: 'b',
+        photo: 'https://picsum.photos/300/300'
+    },
 ]
--- js/roster.js
+++ js/roster.js
@@ -10,9 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     <img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top">
                     <div class="card-body text-center">
                     <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                    <div class="badge badge-position badge-pos-${p.position}">${p.position}</div>
-                    <p class="small text-muted mb-0">Age: ${p.age}</p>
-                    <p class="small text-muted mb-0">Number: ${p.number}</p>
+                    <div class="badge badge-position badge-pos-${p.side}">${p.side}</div>
                     </div>
             </div>
              `
