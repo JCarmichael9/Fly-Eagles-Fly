49fd263063be161abd920888a71b85aad0650a4a
jcarmichael9 <jcarmichael802@gmail.com>
2025-05-30 09:31:28 -0400

    idk

--- js/players.js
+++ js/players.js
@@ -1,122 +1,123 @@
 const players = [
   {
     funFact: "Obi-Wan Kenobi is a Light Side Jedi Master who trained Anakin and fought him before he turned to Darth Vader.",
-    photo: "imgs/obi.png"
+    photo: "imgs/obi.png",
+    firstName: "Obi-Wan"
   },
   {
     funFact: "Anakin Skywalker is a Dark Side Sith Lord formerly a Jedi, destined to become Darth Vader.",
-    photo: "imgs/anakin.png"
+    photo: "imgs/anakin.png",
+    firstName: "Anakin"
   },
   {
     funFact: "Padmé Amidala is a Light Side senator and former queen fighting for peace.",
-    photo: "imgs/padme.png"
+    photo: "imgs/padme.png",
+    firstName: "Padme"
   },
   {
     funFact: "Yoda is a wise Light Side Jedi Master and leader of the Jedi Council.",
-    photo: "imgs/yoda.png"
+    photo: "imgs/yoda.png",
+    firstName: "Yoda"
   },
   {
     funFact: "Darth Vader is a Dark Side Sith Lord and enforcer of the Emperor’s will.",
-    photo: "imgs/vader.jpg"
+    photo: "imgs/vader.jpg",
+    firstName: "Darth"
   },
   {
     funFact: "Mace Windu is a powerful Light Side Jedi Master known for his purple lightsaber.",
-    photo: "imgs/mace.png"
+    photo: "imgs/mace.png",
+    firstName: "Mace"
   },
   {
     funFact: "Count Dooku is a Dark Side Sith Lord and former Jedi who leads the Separatists.",
-    photo: "imgs/dooku.png"
+    photo: "imgs/dooku.png",
+    firstName: "Count"
   },
   {
     funFact: "Qui-Gon Jinn was a Light Side Jedi who discovered Anakin on Tatooine.",
-    photo: "imgs/qui.png"
+    photo: "imgs/qui.png",
+    firstName: "Qui-Gon"
   },
   {
     funFact: "Palpatine is a Dark Side Sith Lord and manipulative Emperor of the galaxy.",
-    photo: "imgs/palp.png"
+    photo: "imgs/palp.png",
+    firstName: "Palpatine"
   },
   {
     funFact: "Leia Organa is a Light Side Rebel leader, princess, and sister to Luke Skywalker.",
-    photo: "imgs/leia.png"
+    photo: "imgs/leia.png",
+    firstName: "Leia"
   },
   {
     funFact: "Han Solo is a Light Side Rebel pilot who helped defeat the Empire.",
-    photo: "imgs/solo.png"
+    photo: "imgs/solo.png",
+    firstName: "Han"
   },
   {
     funFact: "Chewbacca is a loyal Light Side Wookiee warrior and Han Solo’s co-pilot.",
-    photo: "imgs/chewy.png"
+    photo: "imgs/chewy.png",
+    firstName: "Chewbacca"
   },
   {
     funFact: "Lando Calrissian is a Light Side Rebel and skilled gambler turned hero.",
-    photo: "imgs/lando.png"
+    photo: "imgs/lando.png",
+    firstName: "Lando"
   },
   {
     funFact: "Boba Fett is the GREATEST character in the entire series on everything.",
-    photo: "imgs/boba.png"
+    photo: "imgs/boba.png",
+    firstName: "Boba"
   },
   {
     funFact: "Jango Fett is a neutral bounty hunter and template for the clone army.",
-    photo: "imgs/jango.png"
+    photo: "imgs/jango.png",
+    firstName: "Jango"
   },
   {
     funFact: "Darth Maul is a Dark Side Sith apprentice with a double-bladed lightsaber.",
-    photo: "imgs/maul.png"
+    photo: "imgs/maul.png",
+    firstName: "Darth"
   },
   {
     funFact: "C-3PO is a protocol droid fluent in over six million forms of communication.",
-    photo: "imgs/c3p0.png"
+    photo: "imgs/c3p0.png",
+    firstName: "C-3PO"
   },
   {
     funFact: "R2-D2 is a resourceful astromech droid who often saves the heroes.",
-    photo: "imgs/r2d2.png"
+    photo: "imgs/r2d2.png",
+    firstName: "R2-D2"
   },
   {
     funFact: "Jar Jar Binks is a clumsy but well-meaning Gungan ally to the Jedi.",
-    photo: "imgs/jarjar.png"
+    photo: "imgs/jarjar.png",
+    firstName: "Jar-Jar"
   },
   {
     funFact: "General Grievous is a Dark Side cyborg leading Separatist droid armies.",
-    photo: "imgs/grivieous.png"
+    photo: "imgs/grivieous.png",
+    firstName: "Grievous"
   },
   {
     funFact: "Shaak Ti is a wise Togruta Jedi Master who oversaw the training of clone troopers on Kamino.",
-    photo: "imgs/shaak.png"
+    photo: "imgs/shaak.png",
+    firstName: "Shaak"
   },
   {
     funFact: "Captain Rex is a loyal Light Side clone trooper leader under the Jedi.",
-    photo: "imgs/rex.jpg"
+    photo: "imgs/rex.jpg",
+    firstName: "Rex"
   },
   {
     funFact: "B1 Battle Droids are the clumsy yet iconic foot soldiers of the Separatist Droid Army.",
-    photo: "imgs/b1.png"
+    photo: "imgs/b1.png",
+    firstName: "B1"
   },
   {
     funFact: "Watto is a greedy Toydarian junk dealer who owned Anakin and his mother on Tatooine.",
-    photo: "imgs/watto.png"
+    photo: "imgs/watto.png",
+    firstName: "Watto"
   },
-  {
-    funFact: "Boss Nass is the leader of the Gungans and an ally to the Republic.",
-    photo: "imgs/nass.png"
-  },
-  {
-    funFact: "Greedo is a neutral bounty hunter who confronts Han Solo in Mos Eisley.",
-    photo: "imgs/greedo.png"
-  },
-  {
-    funFact: "Jabba the Hutt is a neutral crime lord controlling much of the Outer Rim.",
-    photo: "imgs/jabba.jpg"
-  },
-  {
-    funFact: "Stormtroopers are the backbone soldiers of the Galactic Empire, known for their white armor and questionable aim.",
-    photo: "imgs/stormtrooper.png"
-  },
-  {
-    funFact: "Aayla Secura is a graceful Twi'lek Jedi Master who fought in the Clone Wars and served with distinction.",
-    photo: "imgs/aayla.png"
-  },
-  {
-    funFact: "Admiral Ackbar is a Light Side fleet commander famous for his tactical prowess. It's a trap!",
-    photo: "imgs/ackbar.png"
-  }
-];
+]
+   
--- js/roster.js
+++ js/roster.js
@@ -1,3 +1,21 @@
+function show(name) {
+    const ppl = document.getElementById(name)
+    ppl.classList.add("d-block")
+    ppl.classList.remove("d-none")
+    document.getElementById("noHide").classList.remove("d-none")
+    document.getElementById("hide").classList.add("d-none")
+
+}
+
+function close(name) {
+    const ppl = document.getElementById(name)
+    ppl.classList.remove("d-block")
+    ppl.classList.add("d-none")
+    document.getElementById("noHide").classList.add("d-none")
+    document.getElementById("hide").classList.remove("d-none")
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.getElementById('roster-grid')
     const render = list => {
@@ -6,11 +24,22 @@ document.addEventListener('DOMContentLoaded', () => {
             const col = document.createElement('div')
             col.className = 'col-6 col-lg-2'
             col.innerHTML = `
-                    <img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top rounded-circle border1">
+                    <button onclick="show('${p.firstName}')" class='bg-transparent border-0'><img src="${p.photo}" alt="${p.firstName}'s Beautiful Face" class="card-img-top rounded-circle border1"></button>
              `
-             grid.appendChild(col)
+            grid.appendChild(col)
+            const modal = document.createElement('div')
+            modal.className = 'rounded w-75 display-4 text-white text-center mx-auto border border-1 border-white bg-transparent d-none popIn p-5'
+            modal.id = p.firstName
+            modal.innerHTML = `
+    ${p.firstName} Fun Fact: <br> 
+    <span class="fs-4">${p.funFact}</span> <br> 
+    <button class="fs-2 text-white bg-transparent border border-1 border-white rounded text-center p-3">Close</button>
+    `;
+
+            const button = modal.querySelector("button");
+            button.addEventListener("click", () => close(p.idName));
+            modals.appendChild(modal);
         })
     }
-
     render(players)
 })
